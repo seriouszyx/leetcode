2a869fe3dc4aaac384dac1470c950243942338b2
seriouszyx <674965440@qq.com>
2019-08-05 18:39:28 +0800

    New Problem Solution - "寻找峰值"

---
+++ algorithms/cpp/162.寻找峰值.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=cpp
+ *
+ * [162] 寻找峰值
+ */
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l =  0, r = nums.size() - 1;
+        while (l < r) 
+        {
+            int mid = l + r >> 1;
+            if (nums[mid] > nums[mid + 1]) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+};
+
