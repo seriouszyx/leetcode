170ce248df12705bb6441a112a308409f047aa38
Yixiang Zhao <seriouszyx@foxmail.com>
2021-12-01 20:38:38 +0800

    滑动窗口

---
+++ algorithms/java/438.找到字符串中所有字母异位词.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=438 lang=java
+ *
+ * [438] 找到字符串中所有字母异位词
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        Map<Character, Integer> window = new HashMap<>();
+        Map<Character, Integer> need = new HashMap<>();
+        for (int i = 0; i < p.length(); i++) {
+            char c = p.charAt(i);
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        int left = 0, right = 0;
+        int valid = 0;
+        List<Integer> res = new ArrayList<>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+            while (right - left >= p.length()) {
+                if (valid == need.size()) {
+                    res.add(left);
+                }
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.getOrDefault(d, 0) - 1);
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
